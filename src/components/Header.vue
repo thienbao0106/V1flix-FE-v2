adbc8c0261b5ced7a283f0984c8a0948630c8e4e
thienbao0106 <lamthienbao0106@gmail.com>
2023-09-20 21:56:23 +0700

    first build header

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav
+    class="sticky top-0 bg-bgColor bg-gradient-to-b from-black to-transparent z-20 flex items-center justify-center gap-5 text-white py-4 px-5"
+  >
+    <section class="flex-none w-1/6">Logo</section>
+    <section class="flex-1 w-2/6">Genres</section>
+
+    <section aria-label="search" class="flex flex-col w-3/6 h-12 text-white">
+      <aside aria-label="input" className="flex flex-row">
+        <div
+          class="flex justify-center items-center bg-gray-500 bg-opacity-40 px-2 py-4 rounded-md no-underline text-white gap-3"
+        >
+          <a
+            href="/search"
+            class="no-underline text-white hover:text-secondColorBrighter flex justify-between items-center"
+          >
+            <span class="pi pi-search"></span>
+          </a>
+          <input
+            type="text"
+            class="bg-transparent w-xl px-2 focus:outline-none caret-transparent border-0 text-white placeholder:text-white placeholder:font-bold"
+            placeholder="Search"
+            value=""
+          />
+        </div>
+      </aside>
+    </section>
+  </nav>
+</template>
--- uno.config.ts
+++ uno.config.ts
@@ -3,4 +3,23 @@ import { defineConfig } from "unocss";
 
 export default defineConfig({
   // ...UnoCSS options
+  theme: {
+    colors: {
+      mainColor: "#42484E",
+      secondColor: "#e89b26",
+      secondColorBrighter: "#ecaf51",
+      thirdColor: "#FFECCC",
+      bgColor: "#171d22",
+      detail: "#193445",
+      ongoing: "#007C4E",
+      fistAnime: "#8789C0",
+      secondAnime: "#5DFDCB",
+      thirdAnime: "#7cc6fe",
+      other: "#c2cae8",
+      opacityText: "#22282d",
+    },
+    fontFamily: {
+      mainFont: "Nunito, Arial, sans-serif",
+    },
+  },
 });
