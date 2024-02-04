acf9a465c1b8e52c1feb95b4c4e3d6b020151bd1
Lâm Thiên Bảo <lamthienbao0106@gmail.com>
2024-02-04 17:48:47 +0700

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import UnoCSS from "unocss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), UnoCSS()],
+  server: {
+    host: true,
+    port: 8000, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+    watch: {
+      usePolling: true,
+    },
+  },
 });
