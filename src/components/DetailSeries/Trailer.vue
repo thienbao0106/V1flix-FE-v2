2032d348c25bf11f690ffe835cadc831d0767826
Lâm Thiên Bảo <lamthienbao0106@gmail.com>
2024-04-09 00:02:58 +0700

    fix trailers

--- src/components/DetailSeries/Trailer.vue
+++ src/components/DetailSeries/Trailer.vue
@@ -5,9 +5,9 @@ export default {
 </script>
 
 <template>
-  <div class="w-full">
+  <div v-for="tr in trailer" class="w-full">
     <iframe
-      :src="`https://www.youtube.com/embed/${trailer.id}`"
+      :src="`https://www.youtube.com/embed/${tr.idTrailer}`"
       class="lg:w-[500px] w-full lg:h-[300px] h-[500px]"
       preload="none"
     >
--- src/constants/video.ts
+++ src/constants/video.ts
@@ -57,7 +57,7 @@ export const VIDEO_QUERIES: any = {
     "genres { \n _id \n name \n}",
     "tags { \n _id \n name \n}",
     "duration",
-    "trailer { \n id \n site \n thumbnail \n}",
+    "trailer {  \n site \n thumbnail \n idTrailer \n}",
     "avg_score",
     "rating { \n user { \n _id \n username } \n score \n }",
     "relation { \n role \n related_series { \n _id \n title {\n main_title \n alt_title \n } \n images { \n source \n type \n } \n total_episodes \n status \n } \n}",
